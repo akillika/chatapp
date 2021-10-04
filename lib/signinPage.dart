9c256ab1b3f8944dc14d8595457b4f038afd9058
akillika <akillika49@gmail.com>
2021-10-04 10:53:00 +0530

    sign in page bug fix and chat page buuble additions

--- lib/signinPage.dart
+++ lib/signinPage.dart
@@ -24,143 +24,146 @@ class _SignInPageState extends State<SignInPage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            // crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                "Sign In",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                controller: emailController,
-                // textAlign: TextAlign.center,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: 'Enter you email ID',
-                  hintStyle: TextStyle(fontSize: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
-                    ),
-                  ),
-                  filled: true,
-                  contentPadding: EdgeInsets.all(16),
-                  // fillColor: colorSearchBg,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                controller: passController,
-                // textAlign: TextAlign.center,
-                keyboardType: TextInputType.text,
-                decoration: InputDecoration(
-                  hintText: 'Enter you password',
-                  hintStyle: TextStyle(fontSize: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide(
-                      width: 0,
-                      style: BorderStyle.none,
-                    ),
-                  ),
-                  filled: true,
-                  contentPadding: EdgeInsets.all(16),
-                  // fillColor: colorSearchBg,
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Container(
-                height: 50.0,
-                child: GestureDetector(
-                  onTap: () {
-                    AuthenticationHelper()
-                        .signIn(
-                            email: emailController.text,
-                            password: passController.text)
-                        .then((result) {
-                      if (result == null) {
-                        var currentUser = FirebaseAuth.instance.currentUser;
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => ChatPage(
-                                      uid: currentUser!.uid.toString(),
-                                    )));
-                      } else {
-                        final snackBar = SnackBar(content: Text(result));
-                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                      }
-                    });
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      // border: Border.all(
-                      //   color: Color(0xFFF05A22),
-                      //   // style: BorderStyle.solid,
-                      //   width: 1.0,
-                      // ),
-                      color: Colors.teal,
-                      borderRadius: BorderRadius.circular(30.0),
+                TextField(
+                  controller: emailController,
+                  // textAlign: TextAlign.center,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your email ID',
+                    hintStyle: TextStyle(fontSize: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Center(
-                          child: Text(
-                            "Sign In",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontFamily: 'Montserrat',
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1,
+                    filled: true,
+                    contentPadding: EdgeInsets.all(16),
+                    // fillColor: colorSearchBg,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  obscureText: true,
+                  controller: passController,
+                  // textAlign: TextAlign.center,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                    hintStyle: TextStyle(fontSize: 16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                    filled: true,
+                    contentPadding: EdgeInsets.all(16),
+                    // fillColor: colorSearchBg,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  height: 50.0,
+                  child: GestureDetector(
+                    onTap: () {
+                      AuthenticationHelper()
+                          .signIn(
+                              email: emailController.text,
+                              password: passController.text)
+                          .then((result) {
+                        if (result == null) {
+                          var currentUser = FirebaseAuth.instance.currentUser;
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ChatPage(
+                                        uid: currentUser!.uid.toString(),
+                                      )));
+                        } else {
+                          final snackBar = SnackBar(content: Text(result));
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        }
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        // border: Border.all(
+                        //   color: Color(0xFFF05A22),
+                        //   // style: BorderStyle.solid,
+                        //   width: 1.0,
+                        // ),
+                        color: Colors.teal,
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Center(
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Montserrat',
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1,
+                              ),
                             ),
-                          ),
-                        )
-                      ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => SignUpPage()));
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account?",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    Text(
-                      " Register now",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.teal,
-                          fontSize: 15),
-                    ),
-                  ],
+                SizedBox(
+                  height: 30,
                 ),
-              )
-            ],
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SignUpPage()));
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Text(
+                        " Register now",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.teal,
+                            fontSize: 15),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
